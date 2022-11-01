a86e7b46400b808c1329001b22d57e0b6a730b79
Dillon Brock <mdillon1297@gmail.com>
2022-11-01 09:25:35 -0700

    working fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,9 @@
+Array.from(Array(100).keys())
+  .map(num => num + 1)
+  .map(num => {
+    if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+    else if (num % 3 === 0) return 'fizz';
+    else if (num % 5 === 0) return 'buzz';
+    return num;
+  })
+  .forEach(n => console.log(`${n}\n`));
